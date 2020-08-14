8c591cffcfd2e4ae87a8afdd372d1b5275d19fb8
Fiqry Choerudin <fiqrychoerudin@student.telkomuniversity.ac.id>
2020-08-14 16:40:38 +0700

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Nav></Nav>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Nav from '@/components/Nav.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Nav
+  }
+}
+</script>
